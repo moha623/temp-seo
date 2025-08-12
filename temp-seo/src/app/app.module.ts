74e4a342ef40df319e1e2c6db097a7a41f30cf52
moha623 <chaabanemed84@gmail.com>
2025-08-12 14:21:25 +0100

    vercel-deploy

--- temp-seo/src/app/app.module.ts
+++ temp-seo/src/app/app.module.ts
@@ -13,6 +13,8 @@ import { FooterComponent } from './core/footer/footer.component';
 import { HomeComponent } from './core/home/home.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -24,7 +26,7 @@ import { FormsModule } from '@angular/forms';
   ],
 
   imports: [BrowserModule, AppRoutingModule, CommonModule, FormsModule],
-  providers: [provideClientHydration(withEventReplay())],
+  providers: [provideClientHydration(withEventReplay()), provideFirebaseApp(() => initializeApp({ projectId: "temp-seo", appId: "1:705787828567:web:802c158f1358689f384612", storageBucket: "temp-seo.firebasestorage.app", apiKey: "AIzaSyBB8GO0VJqv3Za01w6F_8ZyUIjzPwVQhbg", authDomain: "temp-seo.firebaseapp.com", messagingSenderId: "705787828567" })), provideFirestore(() => getFirestore())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
